d1f860c0725d69d3d923817b1d5ae9ec191ba630
Oumou09 <oumoudesigns@gmail.com>
2025-04-10 16:51:59 -0400

    updates to the libraryapplication

---
+++ workbook-1/LibraryApplication/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,43 @@
+package com.pluralsight;
+
+public class Main {
+    public static <Int> void main(String[] args) {
+        /* String firstWord = "Hello";
+        String secondWord = "World";
+
+        String greeting = firstWord + secondWord;
+
+        System.out.println(greeting); */
+
+        int firstNumber = 10;
+        int secondNumber = 3;
+        double result;
+
+        //add
+        result = firstNumber + secondNumber;
+        System.out.println(result);
+
+        //subtract
+        result = firstNumber - secondNumber;
+        System.out.println(result);
+
+        //multiplication
+        result = firstNumber * secondNumber;
+        System.out.println(result);
+
+        //division
+        result = (double) firstNumber / secondNumber;
+        System.out.println(result);
+
+        result = firstNumber % secondNumber;
+        System.out.println(result);
+
+
+
+
+
+
+
+    }
+
+    }
